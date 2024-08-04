b8565cf23ceef2f65d21f08b820b9f08f6b9c50b
Hendrik Fick Renaldo Potgieter (Henro) <53427025+henro47@users.noreply.github.com>
2024-08-04 14:33:41 +0200

    Generate index

--- main.go
+++ main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"context"
 	"log"
-	"net/http"
 	"os"
-	"path"
 
-	"github.com/a-h/templ"
 	"github.com/henro47/hfr-go-templ-cv/components"
 )
 
 func main() {
-	wd, err := os.Getwd()
+	/*
+		wd, err := os.Getwd()
+		if err != nil {
+			log.Fatalf("couldn't get current working directory %v", err)
+		}
+		homePage := components.Index()
+		pagesHandler := http.NewServeMux()
+		pagesHandler.Handle("/media/", http.StripPrefix("/media/", http.FileServer(http.Dir(path.Join(wd, "/media/")))))
+		pagesHandler.Handle("/", templ.Handler(homePage))
+		log.Fatalln(http.ListenAndServe(":8080", pagesHandler))*/
+
+	f, err := os.Create("index.html")
 	if err != nil {
-		log.Fatalf("couldn't get current working directory %v", err)
+		log.Fatalf("failed to create output file: %v", err)
 	}
-	homePage := components.Index()
-	pagesHandler := http.NewServeMux()
-	pagesHandler.Handle("/media/", http.StripPrefix("/media/", http.FileServer(http.Dir(path.Join(wd, "/media/")))))
-	pagesHandler.Handle("/", templ.Handler(homePage))
-	log.Fatalln(http.ListenAndServe(":8080", pagesHandler))
+
+	err = components.Index().Render(context.Background(), f)
+	if err != nil {
+		log.Fatalf("failed to write output file: %v", err)
+	}
 }
