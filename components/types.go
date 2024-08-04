bd81d7b52217747606064a1ea827783ac0e09f4a
Hendrik Fick Renaldo Potgieter (Henro) <53427025+henro47@users.noreply.github.com>
2024-08-04 13:39:42 +0200

    templ: Add body

--- components/types.go
+++ components/types.go
@@ -3,11 +3,13 @@ package components
 import "github.com/a-h/templ"
 
 type data struct {
-	Title    string        `json:"title"`
-	SubTitle string        `json:"subtitle"`
-	Image    string        `json:"image"`
-	Github   templ.SafeURL `json:"github`
-	Linkedin templ.SafeURL `json:"linkedin"`
-	Email    templ.SafeURL `json:"email"`
-	Cell     templ.SafeURL `json:"cell"`
+	Title       string        `json:"title"`
+	SubTitle    string        `json:"subtitle"`
+	Description string        `json:"description"`
+	Image       string        `json:"image"`
+	Github      templ.SafeURL `json:"github"`
+	Linkedin    templ.SafeURL `json:"linkedin"`
+	Email       templ.SafeURL `json:"email"`
+	Cell        templ.SafeURL `json:"cell"`
+	Project     templ.SafeURL `json:"project"`
 }
